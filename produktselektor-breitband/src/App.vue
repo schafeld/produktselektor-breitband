55906ccb5215be2ed722223d5ac69d090b3da220
Oliver Schafeld <oliver@schafeld.com>
2020-08-04 10:45:54 +0200

    insert tailwind sample card

--- produktselektor-breitband/src/App.vue
+++ produktselektor-breitband/src/App.vue
@@ -22,6 +22,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: #dddddd;
 }
 
 #nav {
--- produktselektor-breitband/src/views/Home.vue
+++ produktselektor-breitband/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="md:flex bg-white rounded-lg p-6">
+      <img
+        class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
+        src="//picsum.photos/200/300"
+      />
+      <div class="text-center md:text-left">
+        <h2 class="text-lg">Erin Lindford</h2>
+        <div class="text-purple-500">Product Engineer</div>
+        <div class="text-gray-600">erinlindford@example.com</div>
+        <div class="text-gray-600">(555) 765-4321</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
