5333066d1758ef639f2255606ea6104706af919b
Oliver Schafeld <oliver@schafeld.com>
2020-08-04 14:37:38 +0200

    center card on page

--- produktselektor-breitband/src/views/Home.vue
+++ produktselektor-breitband/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home max-w-md mx-auto">
     <div class="md:flex bg-white rounded-lg p-6">
       <img
         class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
