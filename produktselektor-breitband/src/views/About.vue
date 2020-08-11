8b1a349fda5bb0281c708ec0d721b5f5c4613a8d
Oliver Schafeld <oliver@schafeld.com>
2020-08-11 14:38:26 +0200

    add demo Tailwind classes

--- produktselektor-breitband/src/views/About.vue
+++ produktselektor-breitband/src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about bg-pink-100 sm:bg-green-200 md:bg-green-300 lg:bg-green-500 xl:bg-pink-500">
+  <div
+    class="about bg-pink-100 sm:bg-green-200 md:bg-green-300 lg:bg-green-500 xl:bg-pink-500"
+  >
     <h1>This is an about page</h1>
     <p>With some text filler.</p>
   </div>
