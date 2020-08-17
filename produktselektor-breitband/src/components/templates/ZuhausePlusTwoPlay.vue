6561e961617f8d762a9471f625a0082c58a39fea
Oliver Schafeld <oliver@schafeld.com>
2020-08-17 15:10:30 +0200

    use tailwind config extension for Vodafone CI colors

--- produktselektor-breitband/src/components/templates/ZuhausePlusTwoPlay.vue
+++ produktselektor-breitband/src/components/templates/ZuhausePlusTwoPlay.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="panel-layout-two-play" class="flex h-full">
     <div id="main-content" class="w-2/3 bg-gray-200">
-      <h2 class="product-title">ZuhausePlusTwoPlay</h2>
+      <h2 class="product-title text-vodafone-red">ZuhausePlusTwoPlay</h2>
       <p>Aktives Produkt: {{ $store.getters.getActiveProduct[0] }}</p>
     </div>
     <div id="product-selector" class="w-1/3">
       <p>Todo: Add Product selection</p>
       <div class="btn">Klick</div>
+      <p class="text-info bg-vodafone-red inline-block px-2">Roter Text</p>
     </div>
   </div>
 </template>
@@ -15,9 +16,3 @@
 // export default {
 // }
 </script>
-
-<style>
-h2.product-title {
-  color: var(--color-vodafone-red);
-}
-</style>
--- produktselektor-breitband/tailwind.config.js
+++ produktselektor-breitband/tailwind.config.js
@@ -6,7 +6,41 @@ module.exports = {
     './src/**/*.vue'
   ],
   theme: {
-    extend: {},
+    extend: {
+      /* Extend Tailwind with custom css for Vodafone theme. */
+      colors: {
+        'vodafone-grey': '#4A4D4E',
+        'vodafone-red': '#e60000',
+        'vodafone-white': '#ffffff',
+        'vodafone-black': '#000000',
+        'vodafone-aqua-blue': '#00B0CA',
+        'vodafone-aubergine': '#5E2750',
+        'vodafone-turqoise': '#007C92',
+        'vodafone-red-violet': '#9C2AA0',
+        'vodafone-spring-green': '#A8B400',
+        'vodafone-fresh-orange': '#eb9700',
+        'vodafone-lemon-yellow': '#fecb00',
+        'vodafone-dark-grey': '#333333',
+        'vodafone-anthrazite': '#666666',
+        'vodafone-mid-grey': '#999999',
+        'vodafone-platinum': '#afafaf',
+        'vodafone-silver': '#cccccc',
+        'vodafone-aluminum': '#ebebeb',
+        'vodafone-light-grey': '#f4f4f4',
+        'digital-green': '#428600',
+        'digital-dark-red-2': '#820000',
+        'digital-maroon': '#990000',
+        'digital-dark-red': '#bd0000',
+        'digital-red': '#ff0000',
+        'confirmation-green': '#427d00',
+        'info-tooltip': '#f1f8f9',
+        'alert-success': '#f4f7f0',
+        'alert-error': '#FEF0F0',
+        'info-orange': '#ff5400',
+        'alert-warning': '#fff5f0',
+        'info': '#ffffff',
+      }
+    },
   },
   variants: {
     backgroundColor: ['responsive', 'focus', 'hover', 'active'] // optional universal state styles
