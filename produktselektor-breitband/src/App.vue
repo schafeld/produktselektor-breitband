3d868b4a35766bc03c8cd9332f06e2102df3d033
Oliver Schafeld <oliver@schafeld.com>
2020-08-17 11:40:57 +0200

    add topseller route links

--- produktselektor-breitband/src/App.vue
+++ produktselektor-breitband/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/internet-phone-250">2Play Topseller</router-link> |
+      <router-link to="/internet-phone-tv-250">3Play Topseller</router-link>
     </div>
     <router-view />
   </div>
