fba500f6668d99e301ce6a2d25cab69266eb28fb
Oliver Schafeld <oliver@schafeld.com>
2020-08-17 11:53:45 +0200

    add basic 2 to 1 column layout

--- produktselektor-breitband/src/components/templates/ZuhausePlusTwoPlay.vue
+++ produktselektor-breitband/src/components/templates/ZuhausePlusTwoPlay.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
-    <h2 class="product-title">ZuhausePlusTwoPlay</h2>
-    <p>Aktives Produkt: {{ $store.getters.getActiveProduct[0] }}</p>
-    <div class="btn">Klick</div>
+  <div id="panel-layout-two-play" class="flex h-full">
+    <div id="main-content" class="w-2/3 bg-gray-200">
+      <h2 class="product-title">ZuhausePlusTwoPlay</h2>
+      <p>Aktives Produkt: {{ $store.getters.getActiveProduct[0] }}</p>
+    </div>
+    <div id="product-selector" class="w-1/3">
+      <p>Todo: Add Product selection</p>
+      <div class="btn">Klick</div>
+    </div>
   </div>
 </template>
 
