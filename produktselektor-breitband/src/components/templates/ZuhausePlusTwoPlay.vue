545af92ae64a68c3a4ada3c914fd9c3e48f9cca8
Oliver Schafeld <oliver@schafeld.com>
2020-08-24 09:49:10 +0200

    add responsive layout flex/block for feature articles

--- produktselektor-breitband/src/components/templates/ZuhausePlusTwoPlay.vue
+++ produktselektor-breitband/src/components/templates/ZuhausePlusTwoPlay.vue
@@ -10,6 +10,58 @@
       <div class="pb-2/12">
         bla
       </div>
+      <!-- TODO: Turn into organism -->
+      <section>
+        <h2>Deine Vorteile</h2>
+        <!-- TODO: Turn article into/ use molecule 1/3 -->
+        <article class="lg:flex w-full sm:block">
+          <div class="article-text lg:w-1/2 sm:w-full">
+            <h3>Wechseln statt Warten</h3>
+            <p>
+              Wechsel schon heute zu uns und nutze unsere Produkte trotz
+              laufendem DSL-Vertrag sofort. Bis zu 12 Monate für 0 €
+              Grundgebühr.
+            </p>
+          </div>
+          <div
+            class="article-image provider-change bg-red-100 lg:w-1/2 sm:w-full"
+          >
+            placeholder solid color, TODO: Insert proper image.
+          </div>
+        </article>
+        <!-- TODO: Turn article into/ use molecule 2/3 -->
+        <article class="lg:flex w-full sm:block">
+          <div
+            class="article-image provider-change bg-red-100 lg:w-1/2 sm:w-full"
+          >
+            placeholder solid color, TODO: Insert proper image.
+          </div>
+          <div class="article-text lg:w-1/2 sm:w-full">
+            <h3>Wechseln statt Warten</h3>
+            <p>
+              Wechsel schon heute zu uns und nutze unsere Produkte trotz
+              laufendem DSL-Vertrag sofort. Bis zu 12 Monate für 0 €
+              Grundgebühr.
+            </p>
+          </div>
+        </article>
+        <!-- TODO: Turn article into/ use molecule 3/3 -->
+        <article class="lg:flex lg:w-full sm:block">
+          <div class="article-text lg:w-1/2 sm:w-full">
+            <h3>Wechseln statt Warten</h3>
+            <p>
+              Wechsel schon heute zu uns und nutze unsere Produkte trotz
+              laufendem DSL-Vertrag sofort. Bis zu 12 Monate für 0 €
+              Grundgebühr.
+            </p>
+          </div>
+          <div
+            class="article-image provider-change bg-red-100 lg:w-1/2 sm:w-full"
+          >
+            placeholder solid color, TODO: Insert proper image.
+          </div>
+        </article>
+      </section>
     </div>
     <div id="product-selector" class="w-1/3">
       <p>Todo: Add Product selection</p>
