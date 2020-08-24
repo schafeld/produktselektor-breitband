57f2d8a4dc3bec3e45a2cef0d294b774fb1624fd
Oliver Schafeld <oliver@schafeld.com>
2020-08-24 11:35:26 +0200

    add headline styling

--- produktselektor-breitband/src/components/templates/ZuhausePlusTwoPlay.vue
+++ produktselektor-breitband/src/components/templates/ZuhausePlusTwoPlay.vue
@@ -16,7 +16,7 @@
         <!-- TODO: Turn article into/ use molecule 1/3 -->
         <article class="provider-change flex w-full flex-col lg:flex-row">
           <div class="article-text lg:w-1/2 w-full">
-            <h3>Wechseln statt Warten</h3>
+            <h3 class="hl-beta">Wechseln statt Warten</h3>
             <p>
               Wechsel schon heute zu uns und nutze unsere Produkte trotz
               laufendem DSL-Vertrag sofort. Bis zu 12 Monate für 0 €
@@ -35,7 +35,7 @@
             placeholder solid color, TODO: Insert proper image.
           </div>
           <div class="article-text lg:w-1/2 sm:w-full order-1 lg:order-2">
-            <h3>Downgrade-Garantie</h3>
+            <h3 class="hl-beta">Downgrade-Garantie</h3>
             <p>
               Entscheide Dich ob du bis zum 7. Monat Deinen Tarif behalten
               möchtest oder in einen kleineren wechseln willst.
@@ -47,7 +47,9 @@
           class="recommendation-premium flex w-full flex-col lg:flex-row"
         >
           <div class="article-text lg:w-1/2 w-full">
-            <h3>Bis zu 180 € Prämie für Dich und Deinen Freund</h3>
+            <h3 class="hl-beta">
+              Bis zu 180 € Prämie für Dich und Deinen Freund
+            </h3>
             <p>
               Produkt auswählen. Empfehlung versenden. Über Prämie freuen.
             </p>
