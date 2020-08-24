ffc912e1e045e52c893e584b80529022d20c0709
Oliver Schafeld <oliver@schafeld.com>
2020-08-24 11:20:08 +0200

    make advantages section full-responsive with flex only

--- produktselektor-breitband/src/components/templates/ZuhausePlusTwoPlay.vue
+++ produktselektor-breitband/src/components/templates/ZuhausePlusTwoPlay.vue
@@ -14,8 +14,8 @@
       <section>
         <h2>Deine Vorteile</h2>
         <!-- TODO: Turn article into/ use molecule 1/3 -->
-        <article class="lg:flex w-full sm:block">
-          <div class="article-text lg:w-1/2 sm:w-full">
+        <article class="provider-change flex w-full flex-col lg:flex-row">
+          <div class="article-text lg:w-1/2 w-full">
             <h3>Wechseln statt Warten</h3>
             <p>
               Wechsel schon heute zu uns und nutze unsere Produkte trotz
@@ -23,41 +23,36 @@
               Grundgebühr.
             </p>
           </div>
-          <div
-            class="article-image provider-change bg-red-100 lg:w-1/2 sm:w-full"
-          >
+          <div class="article-image bg-red-100 lg:w-1/2 w-full">
             placeholder solid color, TODO: Insert proper image.
           </div>
         </article>
         <!-- TODO: Turn article into/ use molecule 2/3 -->
-        <article class="lg:flex w-full sm:block">
+        <article class="downgrade-option flex w-full flex-col lg:flex-row">
           <div
-            class="article-image provider-change bg-red-100 lg:w-1/2 sm:w-full"
+            class="article-image bg-red-100 lg:w-1/2 w-full order-2 lg:order-1"
           >
             placeholder solid color, TODO: Insert proper image.
           </div>
-          <div class="article-text lg:w-1/2 sm:w-full">
-            <h3>Wechseln statt Warten</h3>
+          <div class="article-text lg:w-1/2 sm:w-full order-1 lg:order-2">
+            <h3>Downgrade-Garantie</h3>
             <p>
-              Wechsel schon heute zu uns und nutze unsere Produkte trotz
-              laufendem DSL-Vertrag sofort. Bis zu 12 Monate für 0 €
-              Grundgebühr.
+              Entscheide Dich ob du bis zum 7. Monat Deinen Tarif behalten
+              möchtest oder in einen kleineren wechseln willst.
             </p>
           </div>
         </article>
         <!-- TODO: Turn article into/ use molecule 3/3 -->
-        <article class="lg:flex lg:w-full sm:block">
-          <div class="article-text lg:w-1/2 sm:w-full">
-            <h3>Wechseln statt Warten</h3>
+        <article
+          class="recommendation-premium flex w-full flex-col lg:flex-row"
+        >
+          <div class="article-text lg:w-1/2 w-full">
+            <h3>Bis zu 180 € Prämie für Dich und Deinen Freund</h3>
             <p>
-              Wechsel schon heute zu uns und nutze unsere Produkte trotz
-              laufendem DSL-Vertrag sofort. Bis zu 12 Monate für 0 €
-              Grundgebühr.
+              Produkt auswählen. Empfehlung versenden. Über Prämie freuen.
             </p>
           </div>
-          <div
-            class="article-image provider-change bg-red-100 lg:w-1/2 sm:w-full"
-          >
+          <div class="article-image bg-red-100 lg:w-1/2 w-full">
             placeholder solid color, TODO: Insert proper image.
           </div>
         </article>
