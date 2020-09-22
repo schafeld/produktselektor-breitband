a936f8a4f6b191590d353514b357f9e0fc1d85af
Oliver Schafeld <oliver@schafeld.com>
2020-09-22 11:52:42 +0200

    remove unused components

--- produktselektor-breitband/src/components/templates/ZuhausePlusTwoPlay.vue
+++ produktselektor-breitband/src/components/templates/ZuhausePlusTwoPlay.vue
@@ -25,56 +25,10 @@
         <!-- TODO: Check if this UI actually works on mobile devices or needs more padding for example. -->
         <sidebar-desktop />
       </div>
-
-      <section>
-        <h2>Deine Vorteile</h2>
-        <!-- TODO: Turn article into/ use molecule 1/3 -->
-        <article class="provider-change flex w-full flex-col lg:flex-row">
-          <div class="article-text lg:w-1/2 w-full">
-            <h3 class="hl-beta">Wechseln statt Warten</h3>
-            <p>
-              Wechsel schon heute zu uns und nutze unsere Produkte trotz
-              laufendem DSL-Vertrag sofort. Bis zu 12 Monate für 0 €
-              Grundgebühr.
-            </p>
-          </div>
-          <div class="article-image bg-red-100 lg:w-1/2 w-full">
-            placeholder solid color, TODO: Insert proper image.
-          </div>
-        </article>
-        <!-- TODO: Turn article into/ use molecule 2/3 -->
-        <article class="downgrade-option flex w-full flex-col lg:flex-row">
-          <div
-            class="article-image bg-red-100 lg:w-1/2 w-full order-2 lg:order-1"
-          >
-            placeholder solid color, TODO: Insert proper image.
-          </div>
-          <div class="article-text lg:w-1/2 sm:w-full order-1 lg:order-2">
-            <h3 class="hl-beta">Downgrade-Garantie</h3>
-            <p>
-              Entscheide Dich ob du bis zum 7. Monat Deinen Tarif behalten
-              möchtest oder in einen kleineren wechseln willst.
-            </p>
-          </div>
-        </article>
-        <!-- TODO: Turn article into/ use molecule 3/3 -->
-        <article
-          class="recommendation-premium flex w-full flex-col lg:flex-row"
-        >
-          <div class="article-text lg:w-1/2 w-full">
-            <h3 class="hl-beta">
-              Bis zu 180 € Prämie für Dich und Deinen Freund
-            </h3>
-            <p>Produkt auswählen. Empfehlung versenden. Über Prämie freuen.</p>
-          </div>
-          <div class="article-image bg-red-100 lg:w-1/2 w-full">
-            placeholder solid color, TODO: Insert proper image.
-          </div>
-        </article>
-      </section>
+      <!-- <responsive-tiles />
       <additional-options />
       <tariff-comparison />
-      <tariff-details />
+      <tariff-details /> -->
     </div>
     <!-- Make this div 'fixed' for a sticky sidebar menu. -->
     <div
@@ -94,9 +48,10 @@
 </template>
 
 <script>
-import AdditionalOptions from "@/components/organisms/AdditionalOptions";
-import TariffComparison from "@/components/organisms/TariffComparison";
-import TariffDetails from "@/components/organisms/TariffDetails";
+// import AdditionalOptions from "@/components/organisms/AdditionalOptions";
+// import TariffComparison from "@/components/organisms/TariffComparison";
+// import TariffDetails from "@/components/organisms/TariffDetails";
+// import ResponsiveTiles from "@/components/molecules/ResponsiveTiles";
 import SidebarDesktop from "@/components/organisms/productselection/SidebarDesktop";
 // import TabsMobile from "@/components/organisms/productselection/TabsMobile";
 import PanelsExistingCustomer from "@/components/organisms/PanelsExistingCustomer";
@@ -106,9 +61,10 @@ import HeroRouter from "@/components/organisms/HeroRouter";
 export default {
   name: "LayoutIntegrator",
   components: {
-    AdditionalOptions,
-    TariffComparison,
-    TariffDetails,
+    // AdditionalOptions,
+    // TariffComparison,
+    // TariffDetails,
+    // ResponsiveTiles,
     SidebarDesktop,
     // TabsMobile,
     PanelsExistingCustomer,
