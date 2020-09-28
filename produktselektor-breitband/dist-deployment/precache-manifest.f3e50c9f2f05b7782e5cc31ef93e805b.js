9e3d7cd7901d1bbaa1ced73cb3810b3c5d593a5c
Oliver Schafeld <oliver.schafeld@unitymedia.de>
2020-09-28 13:16:42 +0200

    add DEV deployment files

---
+++ produktselektor-breitband/dist-deployment/precache-manifest.f3e50c9f2f05b7782e5cc31ef93e805b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10752c553aa75dee075",
+    "url": "/css/app.46483425.css"
+  },
+  {
+    "revision": "a533adcdb2d37f32ea51e1f72dc18550",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d10752c553aa75dee075",
+    "url": "/js/app.64672d11.js"
+  },
+  {
+    "revision": "8e8fb64f39c7a1847e90",
+    "url": "/js/chunk-vendors.411a2cda.js"
+  },
+  {
+    "revision": "b5904eaad87e6bf609c5f91b96230f6b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ produktselektor-breitband/dist-deployment/serviceworker.js
@@ -0,0 +1,3 @@
+importScripts("/precache-manifest.f3e50c9f2f05b7782e5cc31ef93e805b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+
