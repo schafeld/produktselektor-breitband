346f61c8f2a82384953e4c2b4e5eafab6e445703
Oliver Schafeld <oliver.schafeld@unitymedia.de>
2020-09-30 08:07:28 +0200

    add Dev release

---
+++ produktselektor-breitband/dist-deployment/precache-manifest.76d0bb5f9f49a2823b036ef54d454f7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1e179c3c0e7ee416d6",
+    "url": "https://www.unitymedia.de/content/dam/dcomm-unitymedia-de/vodafone/Privatkunden/weichenseite-code/css/app.39bc77a8.css"
+  },
+  {
+    "revision": "0713d828775f5abde2a48ee12f843228",
+    "url": "https://www.unitymedia.de/content/dam/dcomm-unitymedia-de/vodafone/Privatkunden/weichenseite-code/index.html"
+  },
+  {
+    "revision": "8f1e179c3c0e7ee416d6",
+    "url": "https://www.unitymedia.de/content/dam/dcomm-unitymedia-de/vodafone/Privatkunden/weichenseite-code/js/app.997c961c.js"
+  },
+  {
+    "revision": "8e8fb64f39c7a1847e90",
+    "url": "https://www.unitymedia.de/content/dam/dcomm-unitymedia-de/vodafone/Privatkunden/weichenseite-code/js/chunk-vendors.411a2cda.js"
+  },
+  {
+    "revision": "b5904eaad87e6bf609c5f91b96230f6b",
+    "url": "https://www.unitymedia.de/content/dam/dcomm-unitymedia-de/vodafone/Privatkunden/weichenseite-code/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://www.unitymedia.de/content/dam/dcomm-unitymedia-de/vodafone/Privatkunden/weichenseite-code/robots.txt"
+  }
+]);
--- produktselektor-breitband/dist-deployment/serviceworker.js
+++ produktselektor-breitband/dist-deployment/serviceworker.js
@@ -1,3 +1,3 @@
-importScripts("/precache-manifest.9db9bd158a93002b2ba0b9a39ce3e15f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://www.unitymedia.de/content/dam/dcomm-unitymedia-de/vodafone/Privatkunden/weichenseite-code/precache-manifest.76d0bb5f9f49a2823b036ef54d454f7f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
